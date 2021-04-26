96bd371e9ab9905cd30ad07a6ed873fa102b53d7
DenisYakubov <denisyakubov321@gmail.com>
2021-04-26 12:23:55 +0300

    registration part 3 rework

--- app/src/main/java/com/finderestteam/finderest/ui/registation/RegistrationPart3.kt
+++ app/src/main/java/com/finderestteam/finderest/ui/registation/RegistrationPart3.kt
@@ -14,29 +14,39 @@ class RegistrationPart3 : AppCompatActivity() {
     @RequiresApi(Build.VERSION_CODES.N)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.whole_registation)
-        val buu = findViewById<Button>(R.id.button7)
+        setContentView(R.layout.whole_registration_02)
     }
+
+    val bar = findViewById<ProgressBar>(R.id.progressBar5)
+
+    @RequiresApi(Build.VERSION_CODES.N)
     fun button1(view: View){
+        bar.setProgress(1,true)
         val lay1 = findViewById<ConstraintLayout>(R.id.Layout1)
         val lay2 = findViewById<ConstraintLayout>(R.id.Layout2)
         lay1.visibility = View.GONE
         lay2.visibility = View.VISIBLE
     }
 
+    @RequiresApi(Build.VERSION_CODES.N)
     fun button2(view: View){
+        bar.setProgress(2,true)
         val lay2 = findViewById<ConstraintLayout>(R.id.Layout2)
         val lay3 = findViewById<ConstraintLayout>(R.id.Layout3)
         lay2.visibility = View.GONE
         lay3.visibility = View.VISIBLE
     }
+    @RequiresApi(Build.VERSION_CODES.N)
     fun button3(view: View){
+        bar.setProgress(3,true)
         val lay3 = findViewById<ConstraintLayout>(R.id.Layout3)
         val lay4 = findViewById<ConstraintLayout>(R.id.Layout4)
         lay3.visibility = View.GONE
         lay4.visibility = View.VISIBLE
     }
+    @RequiresApi(Build.VERSION_CODES.N)
     fun button4(view: View){
+        bar.setProgress(4,true)
         val no = Toast.makeText(this, "End of the registration", Toast.LENGTH_SHORT)
         no.show()
         val int2 = Intent()

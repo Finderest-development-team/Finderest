7fc9d0115e5da84c276755edf4d5d2621698d5c9
DenisYakubov <denisyakubov321@gmail.com>
2021-04-28 15:32:24 +0300

    registation and main activity binding

--- app/src/main/java/com/finderestteam/finderest/MainActivity.kt
+++ app/src/main/java/com/finderestteam/finderest/MainActivity.kt
@@ -1,29 +1,32 @@
 package com.finderestteam.finderest
 
-import android.os.Bundle
 import android.content.Intent
-import android.widget.Button
-import com.google.android.material.bottomnavigation.BottomNavigationView
+import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.finderestteam.finderest.ui.registation.RegistrationPart1
+import com.finderestteam.finderest.ui.registation.RegistrationPart2
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        val navView: BottomNavigationView = findViewById(R.id.nav_view)
+        setContentView(R.layout.activity_main2)
+        //тут должна будет быть проверна входил ли пользователь в приложение
+        var logIn = true;
+        if(logIn){
+            val int = Intent(this, MainActivity2::class.java)
+            startActivity(int)
+            overridePendingTransition(R.anim.transition_in, R.anim.transition_out)
+        }else{
+            val int2 = Intent(this, RegistrationPart1::class.java)
+            startActivity(int2)
+            overridePendingTransition(R.anim.transition_in, R.anim.transition_out)
+        }
 
-        val navController = findNavController(R.id.nav_host_fragment)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        val appBarConfiguration = AppBarConfiguration(setOf(
-                R.id.navigation_profile, R.id.navigation_find, R.id.navigation_chats))
-        setupActionBarWithNavController(navController, appBarConfiguration)
-        navView.setupWithNavController(navController)
     }
 }
--- app/src/main/java/com/finderestteam/finderest/ui/registation/RegistrationPart1.kt
+++ app/src/main/java/com/finderestteam/finderest/ui/registation/RegistrationPart1.kt
@@ -7,6 +7,7 @@ import android.os.Bundle
 import android.util.Log
 import android.widget.*
 import androidx.annotation.RequiresApi
+import com.finderestteam.finderest.MainActivity2
 import com.finderestteam.finderest.R
 
 class RegistrationPart1 : AppCompatActivity() {
@@ -20,14 +21,25 @@ class RegistrationPart1 : AppCompatActivity() {
             startActivityForResult(int, 1)
             overridePendingTransition(R.anim.transition_in, R.anim.transition_out)
         }
+        val butt2 = findViewById<Button>(R.id.enterButton)
+        butt2.setOnClickListener {
+            val int = Intent(this, MainActivity2::class.java)
+            startActivity(int)
+            overridePendingTransition(R.anim.transition_in, R.anim.transition_out)
+        }
     }
+
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
         if (data == null)
             return
         when (resultCode) {
-            1 -> {Log.d("TAG","A1 $data")}
-            else -> {Log.d("TAG","Smth went wrong")}
+            1 -> {
+                Log.d("TAG", "A1 $data")
+            }
+            else -> {
+                Log.d("TAG", "Smth went wrong")
+            }
         }
     }
 }

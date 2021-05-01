05c29ea4d6b22dc87de30ee8f3224a6644905929
DenisYakubov <denisyakubov321@gmail.com>
2021-05-01 10:54:15 +0300

    some profile stuff 2

--- app/src/main/java/com/finderestteam/finderest/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/finderestteam/finderest/ui/profile/ProfileFragment.kt
@@ -1,14 +1,23 @@
 package com.finderestteam.finderest.ui.profile
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
+import androidx.navigation.NavController
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
 import com.finderestteam.finderest.R
+import com.finderestteam.finderest.ui.registation.RegistrationPart3
+import kotlinx.android.synthetic.main.fragment_profile.view.*
 
 class ProfileFragment : Fragment() {
 
@@ -26,6 +35,8 @@ class ProfileFragment : Fragment() {
         profileViewModel.text.observe(viewLifecycleOwner, Observer {
             textView.text = it
         })
+        root.Button5.setOnClickListener { NavHostFragment.findNavController(this).navigate(R.id.navigation_profile_editor) }
+
         return root
     }
 }

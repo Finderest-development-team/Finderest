4c69a09d783c0bd51f4d4570553cfa4a895c6aa2
DenisYakubov <denisyakubov321@gmail.com>
2021-05-08 14:04:42 +0300

    chat stuff

--- app/src/main/java/com/finderestteam/finderest/ui/chats/ChatsFragment.kt
+++ app/src/main/java/com/finderestteam/finderest/ui/chats/ChatsFragment.kt
@@ -1,5 +1,6 @@
 package com.finderestteam.finderest.ui.chats
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -8,6 +9,7 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProviders
 import com.finderestteam.finderest.R
 import androidx.navigation.fragment.NavHostFragment
+import com.finderestteam.finderest.MainActivity2
 import kotlinx.android.synthetic.main.fragment_chats.view.*
 
 

71e247fb081fd2cf20beb6b7e939595e5e88f48c
Andres Fernandez <andviolin7@gmail.com>
2021-06-29 00:30:47 +0300

    added libraries (groupie, picasso, CircleImageView) and worked in new message activity

---
+++ app/src/main/java/com/finderestteam/finderest/ui/chats/NewMessageActivity.kt
@@ -0,0 +1,65 @@
+package com.finderestteam.finderest.ui.chats
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.finderestteam.finderest.PersonData
+import com.finderestteam.finderest.R
+import com.finderestteam.finderest.ui.chats.LatestMessages
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
+import com.squareup.picasso.Picasso
+import com.xwray.groupie.GroupAdapter
+import com.xwray.groupie.GroupieViewHolder
+import com.xwray.groupie.Item
+import kotlinx.android.synthetic.main.activity_new_message.*
+import kotlinx.android.synthetic.main.user_row_new_message.view.*
+
+class NewMessageActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_new_message)
+
+        supportActionBar?.title = "Select user"
+
+        fetchUsers()
+    }
+
+    private fun fetchUsers() {
+        val ref = FirebaseDatabase.getInstance().getReference("/users")
+        ref.addListenerForSingleValueEvent(object: ValueEventListener {
+
+            override fun onDataChange(p0: DataSnapshot) {
+                val adapter = GroupAdapter<GroupieViewHolder>()
+
+                p0.children.forEach {
+                   val user = it.getValue(PersonData::class.java)
+                   if (user != null) {
+                       adapter.add(UserItem(user!!))
+                   }
+                }
+
+                recyclerview_newmessage.adapter = adapter
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+
+            }
+        })
+    }
+}
+
+class UserItem(val user: PersonData): Item<GroupieViewHolder>() {
+    override fun bind(viewHolder: GroupieViewHolder, position: Int) {
+        viewHolder.itemView.username_textview_new_message.text = user.userName
+        Picasso.get().load(user.profileImageUrl).into(viewHolder.itemView.picture_user_new_message)
+    }
+
+    override fun getLayout(): Int {
+        return R.layout.user_row_new_message
+    }
+}

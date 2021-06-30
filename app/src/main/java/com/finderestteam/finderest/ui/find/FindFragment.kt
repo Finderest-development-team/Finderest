416054bf1ef6802b4e4b1185775974835c338a8d
DenisYAlubov <denisyakubov321@gmail.com>
2021-07-01 01:17:23 +0300

    logic for findPart

--- app/src/main/java/com/finderestteam/finderest/ui/find/FindFragment.kt
+++ app/src/main/java/com/finderestteam/finderest/ui/find/FindFragment.kt
@@ -1,23 +1,19 @@
 package com.finderestteam.finderest.ui.find
 
 import android.annotation.SuppressLint
-import android.net.Uri
+import android.app.ProgressDialog
 import android.os.Bundle
-import android.provider.MediaStore
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.view.animation.BounceInterpolator
 import android.widget.*
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProviders
 import com.finderestteam.finderest.PersonData
 import com.finderestteam.finderest.R
+import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.*
-import com.google.firebase.storage.FirebaseStorage
-import java.util.*
-
+import com.squareup.picasso.Picasso
 
 class FindFragment : Fragment() {
 
@@ -25,7 +21,6 @@ class FindFragment : Fragment() {
     private val listDt = mutableListOf<PersonData>()
     private var it3 = 0
     private val base = FirebaseDatabase.getInstance().getReference("users")
-    private val ref = FirebaseStorage.getInstance().getReference("images")
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -36,11 +31,18 @@ class FindFragment : Fragment() {
                 ViewModelProviders.of(this).get(FindViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_find, container, false)
 
-        getDataBase()
+        getData()
         drawBanner()
 
         root.findViewById<Button>(R.id.LikeButton).setOnClickListener {
-            drawBanner()
+            if (listDt.size <= it3){
+                Toast.makeText(activity, "You have viewed all users", Toast.LENGTH_SHORT).show()
+                it3 = 0
+                drawBanner()
+            }else{
+                drawBanner()
+            }
+
         }
 
         root.findViewById<Button>(R.id.SkipButton).setOnClickListener { }
@@ -48,25 +50,18 @@ class FindFragment : Fragment() {
         return root
     }
 
-    private fun getDataBase(){
-        base.addValueEventListener(object: ValueEventListener{
+    private fun getData(){
+        base.addValueEventListener(object : ValueEventListener {
             override fun onCancelled(error: DatabaseError) {
                 Toast.makeText(activity, "error: cant keep up", Toast.LENGTH_SHORT).show()
             }
 
             override fun onDataChange(snapshot: DataSnapshot) {
-                snapshot?.children?.forEach {
-                        element -> listDt.add(element.getValue(PersonData::class.java)!!)
+                snapshot?.children?.forEach { element ->
+                    listDt.add(element.getValue(PersonData::class.java)!!)
                 }
             }
         })
-    }
-    private fun getAndSetPhoto(mail: String): Boolean {
-        val path = ref.child("$mail").downloadUrl
-        path.addOnSuccessListener{
-            activity?.findViewById<ImageView>(R.id.bannerPhoto)?.setImageURI(it)
-        }
-        return path.isSuccessful
     }
 
     @SuppressLint("CutPasteId")
@@ -76,17 +71,23 @@ class FindFragment : Fragment() {
             Toast.makeText(activity, "There are no users yet", Toast.LENGTH_SHORT).show()
             return
         }
+        if(FirebaseAuth.getInstance().currentUser.email == listDt[it3].userMail){
+            it3++
+        }
+
         if (listDt.size <= it3){
             Toast.makeText(activity, "You have viewed all users", Toast.LENGTH_SHORT).show()
-            it3 = 0
+            return
         }
 
         val listView = activity?.findViewById<ListView>(R.id.BannerListOfInterests1)
         val name = listDt[it3].userName
         val mail = listDt[it3].userMail
         val arr = listDt[it3].userListOfInterests?.split(" ")?.toTypedArray()
+        val uiUser = listDt[it3].profileImageUrl
 
-        if(name != null && mail != null && arr != null /*&& getAndSetPhoto(mail)*/){
+        if(name != null && mail != null && arr != null && uiUser != null){
+            Picasso.get().load(uiUser).into(activity?.findViewById<ImageView>(R.id.bannerPhoto))
             activity?.findViewById<TextView>(R.id.bannerPersonName)?.text = name
             activity?.findViewById<TextView>(R.id.bannerPersonMail)?.text = mail
             val adapter = activity?.let { it2 -> ArrayAdapter<String>(it2, android.R.layout.simple_list_item_1, arr) }

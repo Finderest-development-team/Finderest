9f0f02cfdf1b3506f0280764ee59f27ffd85579f
Andres Fernandez <andviolin7@gmail.com>
2021-06-30 17:36:03 +0300

    changed database user identificator to uid and worked in chat log

--- app/src/main/java/com/finderestteam/finderest/PersonData.kt
+++ app/src/main/java/com/finderestteam/finderest/PersonData.kt
@@ -1,9 +1,12 @@
 package com.finderestteam.finderest
 
-class PersonData(
-    var uid: String? = null,
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
+class PersonData(var uid: String? = null,
     var userName: String? = null,
     var userMail: String? = null,
     var userPassword: String? = null,
     var profileImageUrl: String? = null,
-    var userListOfInterests: String? = null) {}
+    var userListOfInterests: String? = null): Parcelable{}
--- app/src/main/java/com/finderestteam/finderest/ui/chats/Message.kt
+++ app/src/main/java/com/finderestteam/finderest/ui/chats/Message.kt
@@ -1,18 +1,8 @@
 package com.finderestteam.finderest.ui.chats
-import com.google.firebase.auth.FirebaseUser
-import java.util.Date
-class Message(_userName:String, _textMessage:String) {
-    private val messageTime:Long = Date().time
-    private val userName:String = _userName
-    private val textMessage:String = _textMessage
-    fun getName(): String {
-        return this.userName
-    }
-    fun getMessage(): String {
-        return this.textMessage
-    }
-    fun getTime(): Long {
-        return this.messageTime
-    }
 
-}
+
+class Message(var id: String? = null,
+              var text: String? = null,
+              var fromId: String? = null,
+              var toId: String? = null,
+              var timestamp: Long? = null){}

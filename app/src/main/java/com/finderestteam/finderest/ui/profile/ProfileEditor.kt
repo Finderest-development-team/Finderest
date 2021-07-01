c2590da3d237ae565ffb85717510ab93ae46060f
DenisYAlubov <denisyakubov321@gmail.com>
2021-07-01 18:06:25 +0300

    added app logo and profile is done

--- app/src/main/java/com/finderestteam/finderest/ui/profile/ProfileEditor.kt
+++ app/src/main/java/com/finderestteam/finderest/ui/profile/ProfileEditor.kt
@@ -6,10 +6,13 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageButton
+import android.widget.TextView
 import androidx.core.view.ViewCompat
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.NavHostFragment
 import com.finderestteam.finderest.R
+import com.finderestteam.finderest.ui.chats.LatestMessages
+import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.fragment_profile.view.*
 import kotlinx.android.synthetic.main.fragment_profile.view.Button5
 import kotlinx.android.synthetic.main.fragment_profile_editor.view.*
@@ -24,6 +27,15 @@ class ProfileEditor:Fragment() {
         val root = inflater.inflate(R.layout.fragment_profile_editor, container, false)
         sharedElementEnterTransition = TransitionInflater.from(context).inflateTransition(android.R.transition.move)
         root.button11.setOnClickListener { NavHostFragment.findNavController(this).navigate(R.id.navigation_profile) }
+
+        val imageView = root.findViewById<ImageButton>(R.id.imageButton)
+        val nameView  = root.findViewById<TextView>(R.id.textView8)
+        val mailView  = root.findViewById<TextView>(R.id.textView)
+
+        Picasso.get().load(LatestMessages.currentUser?.profileImageUrl).into(imageView)
+        nameView.text = LatestMessages.currentUser?.userName
+        mailView.text = LatestMessages.currentUser?.userMail
+
         return root
     }
 }
--- app/src/main/java/com/finderestteam/finderest/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/finderestteam/finderest/ui/profile/ProfileFragment.kt
@@ -5,12 +5,24 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageButton
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProviders
 import androidx.navigation.fragment.FragmentNavigatorExtras
 import androidx.navigation.fragment.NavHostFragment
+import com.finderestteam.finderest.PersonData
 import com.finderestteam.finderest.R
+import com.finderestteam.finderest.ui.chats.LatestMessages
+import com.finderestteam.finderest.ui.chats.Message
+import com.finderestteam.finderest.ui.chats.MessageFromItem
+import com.finderestteam.finderest.ui.chats.MessageToItem
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.*
+import com.squareup.picasso.Picasso
+import kotlinx.android.synthetic.main.activity_chat_log.*
+import kotlinx.android.synthetic.main.activity_main2.*
 import kotlinx.android.synthetic.main.fragment_profile.view.*
 
 class ProfileFragment : Fragment() {
@@ -25,7 +37,14 @@ class ProfileFragment : Fragment() {
         profileViewModel =
                 ViewModelProviders.of(this).get(ProfileViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_profile, container, false)
+
         val imageView = root.findViewById<ImageButton>(R.id.imageButton)
+        val nameView  = root.findViewById<TextView>(R.id.textView4)
+
+        Picasso.get().load(LatestMessages.currentUser?.profileImageUrl)
+            .into(imageView)
+        nameView.text = LatestMessages.currentUser?.userName
+
         root.Button5.setOnClickListener {
             //оно не работает как надо, нет анимации я хз почему
             val extras = FragmentNavigatorExtras(
